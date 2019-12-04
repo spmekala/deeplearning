84219a92a95af5cbc5a677275c97c3cb424f945f
spmekala <spmekala@gmail.com>
2019-12-03 23:52:58 -0500

    first commit

---
+++ user_constants.py
@@ -0,0 +1,11 @@
+# Path to dataset 
+DATA_PATH_DICT = {
+    'CIFAR': '/tmp/',
+    'RestrictedImageNet': '/scratch/engstrom_scratch/imagenet',
+    'ImageNet': '/scratch/engstrom_scratch/imagenet',
+    'H2Z': '/home/jupyter/robustness_applications/horse2zebra',
+    'A2O': '/home/jupyter/robustness_applications/apple2orange',
+    'S2W': '/home/jupyter/robustness_applications/summer2winter_yosemite'
+    
+    
+}
